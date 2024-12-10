8ab23bdda1b4a551fcdeb271ab24ab077bacab0a
vaquecar <vaquecar@students.zhaw.ch>
2024-12-10 22:19:31 +0100

    only reveal at the very end

--- blockjack.sol
+++ blockjack.sol
@@ -3,9 +3,6 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
-/// @title TODO
-/// @author TODO
-/// @notice TODO
 contract BlockJack {
     uint8 private constant ZERO_INDEX_SHIFT = 1;
     uint8 private constant MAX_CARD_VALUE = 10;
@@ -19,6 +16,7 @@ contract BlockJack {
     event Hit(address indexed player);
     event Stand(address indexed player);
     event Bust(address indexed player);
+    event Win(address indexed player);
 
     address[] private players;
     mapping(address => Card[]) private hands;
@@ -47,7 +45,7 @@ contract BlockJack {
     function deal() public {
         require(msg.sender == dealer, "Only the dealer can deal.");
         require(
-             haveAllPlayersDecided() || block.timestamp >= currentRoundTimeout,
+            haveAllPlayersDecided() || block.timestamp >= currentRoundTimeout,
             "Current round is still running."
         );
         require(
@@ -60,12 +58,12 @@ contract BlockJack {
             phase = Phase.HitOrStand;
         } else if (phase == Phase.HitOrStand) {
             handleRoundForPlayers();
-            dealerReveal();
         }
 
         if (phase == Phase.HitOrStand) {
             currentRoundTimeout = block.timestamp + roundSessionExpiryInSeconds;
-        } else if (phase == Phase.DealersBust) {
+        } else if (phase == Phase.PlayersStand) {
+            dealerReveal();
             resetState();
         } else if (phase == Phase.PlayersBust) {
             resetState();
@@ -118,6 +116,8 @@ contract BlockJack {
     }
 
     function handleRoundForPlayers() private {
+        uint playersBust = 0;
+        uint playersStand = 0;
         uint playersThatCanContinuePlaying = players.length;
         for (uint i = 0; i < players.length; i++) {
             address playerAddress = players[i];
@@ -132,11 +132,15 @@ contract BlockJack {
 
             if (playerStatus[playerAddress] == PlayerStatus.Stand) {
                 //players that will no longer be able to play have the status PlayerStatus.Stand
-                playersThatCanContinuePlaying -= 1;
+                playersStand += 1;
+            } else if (playerStatus[playerAddress] == PlayerStatus.Bust) {
+                playersBust += 1;
             }
         }
-        if (playersThatCanContinuePlaying == 0) {
+        if (playersBust == players.length) {
             phase = Phase.PlayersBust;
+        }else if(playersBust + playersStand == players.length){
+            phase = Phase.PlayersStand;
         }
     }
 
@@ -146,7 +150,7 @@ contract BlockJack {
         // sum of player cards is higher than BLACK_JACK
         if (sumOfHand(hands[playerAddress]) > BLACK_JACK) {
             emit Bust(playerAddress);
-            playerStatus[playerAddress] = PlayerStatus.Stand;
+            playerStatus[playerAddress] = PlayerStatus.Bust;
         } else {
             playerStatus[playerAddress] = PlayerStatus.NeedsToDecide;
         }
@@ -159,8 +163,18 @@ contract BlockJack {
                 hands[dealer].push(getCard());
             }
             if (sumOfHand(hands[dealer]) > BLACK_JACK) {
+                notifyPlayersThatWon();
                 emit Bust(dealer);
-                phase = Phase.DealersBust;
+            }else{
+                emit Win(dealer);
+            }
+        }
+    }
+
+    function notifyPlayersThatWon() private {
+        for (uint i = 0; i < players.length; i++) {
+            if (playerStatus[players[i]] == PlayerStatus.Stand) {
+                emit Win(players[i]);
             }
         }
     }
@@ -221,12 +235,13 @@ contract BlockJack {
         PlaceBets,
         HitOrStand,
         PlayersBust,
-        DealersBust
+        PlayersStand
     }
 
     enum PlayerStatus {
         NotPlaying,
         NeedsToDecide,
+        Bust,
         Hit,
         Stand
     }
